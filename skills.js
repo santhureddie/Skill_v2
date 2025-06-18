021b3f81860bae17a03c049b9e4c5a8320a2e81e
Santhosh Kumar Reddy Jampana <61430089+santhureddie@users.noreply.github.com>
2025-06-18 00:49:03 -0500

    Add files via upload

---
+++ skills.js
@@ -0,0 +1,11 @@
+window.skillKeywords = [
+  "Python", "Java", "JavaScript", "TypeScript", "Go", "C++", "C#", "Erlang",
+  "Docker", "Kubernetes", "Terraform", "Jenkins", "CI/CD", "Git", "GitHub",
+  "Bitbucket", "Linux", "Shell", "SQL", "NoSQL", "MongoDB", "PostgreSQL",
+  "MySQL", "Django", "Node.js","Node", "Next.js","Next", "React", "Vue", "Angular",
+  "AWS", "Azure", "GCP", "REST", "GraphQL", "Kafka", "Agile", "Scrum",
+  "Jira", "Prometheus", "AI Engineer", "Grafana", "Android", "AI", "Machine Learning",
+  "STT", "Speech to Text", "TTS", "Text to Speech", "VAD", "Voice Activity Detection",
+  "LLM", "Large Language Model", "ChatGPT", "OpenAI", "GPT-3", "GPT-4", "BERT", "Transformer",
+  "NLP", "Natural Language Processing", "Deep Learning", "Neural Networks",
+];
